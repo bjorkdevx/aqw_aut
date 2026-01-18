7916a6bf16ad290a3d77292e41ebef41e8b261a3
Bjork <philip@skogsbrynet10.se>
2026-01-18 17:20:53 +0100

    up

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def run_rotation_imperial_chunin_farming(rotation):
 
                 if random.randint(1,2) == 1:
                     print("get money")
-                    print((SEVEN_MONEY_1_X, SEVEN_MONEY_1_Y, SEVEN_MONEY_2_X, SEVEN_MONEY_2_Y)
+                    print(SEVEN_MONEY_1_X, SEVEN_MONEY_1_Y, SEVEN_MONEY_2_X, SEVEN_MONEY_2_Y)
                     pyautogui.leftClick(SEVEN_MONEY_1_X, SEVEN_MONEY_1_Y)
                     time.sleep(2)
                     pyautogui.leftClick(SEVEN_MONEY_2_X, SEVEN_MONEY_2_Y)
